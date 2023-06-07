d0a5b8d59523ef416011d08c3e58996fae425e6d
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-06-07 11:46:45 -0700

    Added SearchBox

---
+++ src/SearchBox.js
@@ -0,0 +1,13 @@
+function SearchBox() {
+    return (
+        <div className='pa2'>
+            <input
+                className='pa3 ba bg-black-80 bg-light-yellow'
+                type='search'
+                placeholder='Search Players'
+            />
+        </div>
+    );
+}
+
+export default SearchBox;
